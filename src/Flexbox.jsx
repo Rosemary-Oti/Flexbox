737c73a1b596f7e0f365f07dd487430969db07b6
Rosemary-Oti <otirosemary58@gmail.com>
2024-04-28 18:30:32 +0100

    my Flexbox work

---
+++ src/Flexbox.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Flexbox.css";
+
+function Flexbox() {
+    return (
+        <div className="container_flex">
+
+            <div className="grid_container">
+                <h1 className="grid1">Rose</h1>
+                <h1 className="grid2">Mary</h1>
+                <h1 className="grid3">Ruth</h1>
+                <h1 className="grid4">John</h1>
+                <h1 className="grid5">Rose</h1>
+                <h1 className="grid6">Mary</h1>
+                <h1 className="grid7">Ruth</h1>
+                <h1 className="grid8">John</h1>
+                <h1 className="grid9">Rose</h1>
+                <h1 className="grid10">Mary</h1>
+                <h1 className="grid11">Ruth</h1>
+                <h1 className="grid12">John</h1>
+                <h1 className="grid13">Rose</h1>
+                <h1 className="grid14">Mary</h1>
+                <h1 className="grid15">Ruth</h1>
+                <h1 className="grid16">John</h1>
+
+            </div>
+        </div>
+    );
+}
+
+export default Flexbox;
